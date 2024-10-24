fdcf113989ed1086ed255d7d705c58dd59e5df51
1alusha1 <alusha9636@gmail.com>
2024-10-25 01:44:48 +0300

    updated cors setings

--- index.js
+++ index.js
@@ -8,7 +8,12 @@ import postRoutes from "./routes/postRoutes.js";
 dotenv.config();
 
 const app = express();
-app.use(cors());
+app.use(
+  cors({
+    origin: "https://dall-e-clone-client.netlify.app",
+    methods: "GET,POST,PUT,DELETE",
+  })
+);
 app.use(express.json({ limit: "50mb" }));
 
 app.use("/api/v1/post", postRoutes);
